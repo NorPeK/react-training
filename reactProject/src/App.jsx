199548598c308ab36802e64984e2ef85a594a46f
NorPeK <norpekbusiness@hotmail.com>
2024-10-21 13:56:22 +0300

    trying out react

---
+++ reactProject/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header.jsx"
+import Footer from "./components/footer.jsx";
+import Food from "./components/food.jsx";
+function App() {
+
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App
