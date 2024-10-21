12a9cd13a5ceb8fb91619428d6de9a93dcd6034a
NorPeK <norpekbusiness@hotmail.com>
2024-10-21 15:57:52 +0300

    conditional rendering

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -4,13 +4,12 @@ import Food from "./components/food.jsx";
 import Card from "./components/Card.jsx";
 import Button from "./components/button.jsx";
 import Student from "./components/Student.jsx";
+import UserGreeting from "./components/UserGreeting.jsx";
 function App() {
 
   return(
     <>
-    <Student name="NorPeK" age={30} isStudent={false}/>
-    <Student name="Rami" age={21} isStudent={true}/>
-    <Student/>
+    <UserGreeting isLoggedIn = {true} username = "NorPeK"/>
     </>
   );
 }
