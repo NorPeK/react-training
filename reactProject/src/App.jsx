73e470446fd4c1c09f0a263c73006bd3bece462b
NorPeK <norpekbusiness@hotmail.com>
2024-10-21 15:21:15 +0300

    props

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -2,11 +2,15 @@ import Header from "./components/header.jsx"
 import Footer from "./components/footer.jsx";
 import Food from "./components/food.jsx";
 import Card from "./components/Card.jsx";
+import Button from "./components/button.jsx";
+import Student from "./components/Student.jsx";
 function App() {
 
   return(
     <>
-    <Card/>
+    <Student name="NorPeK" age={30} isStudent={false}/>
+    <Student name="Rami" age={21} isStudent={true}/>
+    <Student/>
     </>
   );
 }
